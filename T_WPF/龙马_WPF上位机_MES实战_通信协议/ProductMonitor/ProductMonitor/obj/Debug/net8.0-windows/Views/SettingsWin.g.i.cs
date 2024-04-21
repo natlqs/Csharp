30b2b26608e30765f1e8a3e7858537c272c26d81
natlqs <natlqs@outlook.com>
2024-04-21 08:11:28 +0800

    a

--- T_WPF/龙马_WPF上位机_MES实战_通信协议/ProductMonitor/ProductMonitor/obj/Debug/net8.0-windows/Views/SettingsWin.g.i.cs
+++ T_WPF/龙马_WPF上位机_MES实战_通信协议/ProductMonitor/ProductMonitor/obj/Debug/net8.0-windows/Views/SettingsWin.g.i.cs
@@ -61,7 +61,7 @@ namespace ProductMonitor.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ProductMonitor;V1.0.0.0;component/views/settingswin.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProductMonitor;component/views/settingswin.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\SettingsWin.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
