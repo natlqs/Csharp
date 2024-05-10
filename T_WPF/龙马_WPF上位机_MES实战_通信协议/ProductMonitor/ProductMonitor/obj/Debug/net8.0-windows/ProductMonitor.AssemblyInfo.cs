82f17f3f9b379b4e9beafc44a777aacacc9213a9
natlqs <natlqs@outlook.com>
2024-05-10 13:30:31 +0800

    a

--- T_WPF/龙马_WPF上位机_MES实战_通信协议/ProductMonitor/ProductMonitor/obj/Debug/net8.0-windows/ProductMonitor.AssemblyInfo.cs
+++ T_WPF/龙马_WPF上位机_MES实战_通信协议/ProductMonitor/ProductMonitor/obj/Debug/net8.0-windows/ProductMonitor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef8ecd2ae0ceda5b85e239762a8a5cc14c583046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
